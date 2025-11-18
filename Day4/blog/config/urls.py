1e4920b5d3dd2c21dd160b210af49362d09a002c
김민주 <minjuju518@naver.com>
2025-11-18 22:53:44 +0900

    ✨Day3: Blog2

--- Day4/blog/config/urls.py
+++ Day4/blog/config/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('todo/', include('todo.urls'))
 """
-from xml.etree.ElementInclude import include
 
 from django.contrib import admin
 from django.urls import path,include
