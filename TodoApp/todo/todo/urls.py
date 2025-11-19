0920ca6d7db0202fac885935d2e0f736fd13af07
김민주 <minjuju518@naver.com>
2025-11-19 18:39:40 +0900

    :dizzy: Day5: TodoApp (CBV)

---
+++ TodoApp/todo/todo/urls.py
@@ -0,0 +1,14 @@
+#CBV
+
+from django.urls import path
+from todo import cb_views
+
+app_name = 'todo'
+
+urlpatterns = [
+    path('', cb_views.TodoListView.as_view(), name='list'),
+    path('<int:pk>/', cb_views.TodoDetailView.as_view(), name='detail'),
+    path('create/', cb_views.TodoCreateView.as_view(), name='create'),
+    path('<int:pk>/update/', cb_views.TodoUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', cb_views.TodoDeleteView.as_view(), name='delete'),
+]
